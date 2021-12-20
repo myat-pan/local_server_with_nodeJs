33f397445cd2da5cda8d9e30665c90d450134f9b
mppz <user@mppzs-MacBook-Pro.local>
2021-12-20 22:43:33 +0630

    added callback url for payment in server

---
+++ nodejs_server/server.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+app.use(bodyParser.json({ type: 'application/json' }));
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const statusOK = 200;
+const statusNotFound = 404;
+
+var responseCode = {
+    "responseCode": "0000",
+    "responseMsg": "Operation Success"
+}
+
+var mockDatabase = [
+    {
+        fruit: "apple",
+        color: "red"
+    },
+    {
+        fruit: "banana",
+        color: "yellow"
+    }
+]
+
+var json = {
+    "authenToken": "3f783751c9ef36b21bb7035776ee8ca8daf886f463c4bc4527abc1b1e07c60c599c70904da9014b65da5ee935db0c2c8b62624cb2661c5539748734f86090556",
+    "ecommerceId": "W010101",
+    "transactionType": "0",
+    "orderId": "AA1111",
+    "orderDetails": "CB Test Order 001",
+    "amount": "800.00",
+    "currency": "MMK",
+    "notifyUrl": "https://f0ee-185-107-95-229.ngrok.io/callback",
+    "signature": "4e595bc1a4bb00dfc0f0ab9f97a204a7ef15cf5a1dcf6ad6e396ceecb531f259",
+    "subMerId": "0000000001700001"
+}
+
+
+
+//Handle callback Url
+app.post('/:callback', (req, res) => {
+    res.send(responseCode);
+    const theData = res.body;
+    console.log(theData);
+    console.log(req.body);
+});
+
+//Handle GET request
+app.get('/', function (req, res) {
+    if (mockDatabase.length < 1) {
+        res.statusCode = statusNotFound;
+        res.send('Item not found');
+        return;
+    }
+    res.statusCode = statusOK;
+    res.send(json);
+});
+
+//Handle Get(one)request
+app.get('/:id', function (req, res) {
+    var id = req.params.id;
+    if (id < 0 || id >= mockDatabase.length) {
+        res.statusCode = statusNotFound;
+        res.send('Item not found');
+        return;
+    }
+    res.statusCode = statusOK;
+    res.send(mockDatabase[id]);
+});
+
+//Handle POST request
+app.post('/', function (req, res) {
+    var newObject = req.body;
+    mockDatabase.push(newObject);
+    var id = mockDatabase.length - 1;
+    res.statusCode = statusOK;
+    res.send(`Item ad with id${id}`);
+});
+
+//Handle DELETE request
+app.delete('/:id', function (req, res) {
+    var id = req.params.id;
+    mockDatabase.splice(id, 1);
+    res.statusCode = statusOK;
+    res.send(`Item deleted at id ${id}`);
+});
+
+app.listen(port, hostname, function () {
+    console.log(`Listening at http://${hostname}:${port}/...`)
+});
+
+
+
